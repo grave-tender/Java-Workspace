e68bb68161eee89c3d61f3605acf23119eb0ef16
grave-tender <carlos.leon@hotmail.fr>
2022-02-14 13:41:11 -0500

    lundi 14 fev

---
+++ tableaux/src/Tableaux.java
@@ -0,0 +1,13 @@
+public class Tableaux {
+    public static void main(String[] args) {
+        String[] cat = new String[2];
+
+        cat[0] = "Daisy";
+        cat[1] = "Livi";
+
+        for (String i : cat){
+            System.out.println(i);
+        }
+    }
+    
+}
