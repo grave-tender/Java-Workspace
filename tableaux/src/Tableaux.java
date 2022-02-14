2c054afa2e28e82ee49d8ff413f61f907de79488
grave-tender <carlos.leon@hotmail.fr>
2022-02-14 14:16:46 -0500

    exercise

--- tableaux/src/Tableaux.java
+++ tableaux/src/Tableaux.java
@@ -1,12 +1,10 @@
 public class Tableaux {
     public static void main(String[] args) {
         int nb[] = {15,20,10,5,35,14};
-        int max = 0;
         int posMax = 0;
 
         for (int i = 0; i < nb.length; i++){
-            if(max < nb[i]){
-                max = nb[i];
+            if(nb[posMax] < nb[i]){
                 posMax = i;
             }
         }
