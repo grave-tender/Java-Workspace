150427ccb75b58bda97629a82befc559e1eca3a2
grave-tender <carlos.leon@hotmail.fr>
2022-02-14 14:12:04 -0500

    exercice

--- tableaux/src/Tableaux.java
+++ tableaux/src/Tableaux.java
@@ -1,13 +1,16 @@
 public class Tableaux {
     public static void main(String[] args) {
-        String[] cat = new String[2];
+        int nb[] = {15,20,10,5,35,14};
+        int max = 0;
+        int posMax = 0;
 
-        cat[0] = "Daisy";
-        cat[1] = "Livi";
-
-        for (String i : cat){
-            System.out.println(i);
+        for (int i = 0; i < nb.length; i++){
+            if(max < nb[i]){
+                max = nb[i];
+                posMax = i;
+            }
         }
+
+        System.out.println("La valeur maximale sur le tableaux est a l'indice " + posMax + " et est a la valeur de " + nb[posMax]);
     }
-    
 }
