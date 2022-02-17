a0ea918d82a20daf490b6c6775385531d32923c6
grave-tender <carlos.leon@hotmail.fr>
2022-02-17 14:38:08 -0500

    petite correction

--- tableaux/src/Tableaux.java
+++ tableaux/src/Tableaux.java
@@ -4,7 +4,8 @@ public class Tableaux {
         int posMax = 0;
 
         for (int i = 0; i < nb.length; i++){
-            if(nb[posMax] < nb[i]) posMax = i; 
+            if(nb[posMax] < nb[i])
+                posMax = i;
         }
 
         System.out.println("La valeur maximale sur le tableaux est a l'indice " + posMax + " et est a la valeur de " + nb[posMax]);
